d57c31c5c823a083e17d5db4adefd9e29d3fb547
khurana <swamini.khurana@ufz.de>
2020-07-21 13:31:59 +0200

    reading BRNS files update

--- Read_BRNS_files.py
+++ Read_BRNS_files.py
@@ -15,7 +15,7 @@ ratenames = proc.masterrates("saturated")
 
 #look for file names
 Chems = list(values['Var'] for t,values in chemdict.items())
-chemindx = list(values['TecIndex']-5 for t,values in chemdict.items())
+chemindx = list(values['TecIndex']-7 for t,values in chemdict.items())
 microbes = list(values['Var'] for t,values in microdict.items())
 microbeindx = list(values['TecIndex']-5 for t,values in microdict.items())
 
@@ -32,11 +32,11 @@ respindx = list(ratenames.index(i)+1 for i in ratenames)
 ratefiles = list("xrate"+str(i) for i in respindx)
 
 #Path to file
-path_data = "X:\Pap1_discussion\BRNS_PeDa_12\conc.dat"
+path_data = "X:\Pap1_discussion\BRNS_PeDa_2\conc.dat"
 columnstoread = chemindx.append(microbeindx) #WIP not working yet
 
 data = np.loadtxt(path_data, skiprows = 1)#, usecols = chemindx)
 np.shape(data)
 
-delta = (data[0,1:] - data[-1,1:])/data[0,1:]
-delta[chemindx[1]]
+delta = (data[-1,1:])/data[0,1:]
+delta[chemindx[3]-1]
