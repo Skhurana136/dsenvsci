e0283bf61d21458576828e59fc42979fc65d94dc
Skhurana <62461235+Skhurana136@users.noreply.github.com>
2021-12-03 17:22:45 +0100

    Check again if carbon loop is complete

--- solvers/diff_eqns_system.py
+++ solvers/diff_eqns_system.py
@@ -15,7 +15,7 @@ c_mol_bio = 10 #carbon atoms in microbial necromass
 def call_constants():
     params = [vparams, kparams, yparams, mparams]
     return params
- # %%
+
 def rates(x,t, c_n, b_n):
     
     # constants
@@ -31,31 +31,53 @@ def rates(x,t, c_n, b_n):
 
     # define rates for:
     # 1. respiration
+    # formula to implement for all B,C combinations:
+    # r = v*B*C/(k+C). v,k are unique to each B,C combination
     r_resp = v_params.reshape(c_n,b_n)*np.multiply(B,C[...,None])/(k_params.reshape(c_n, b_n)+C[...,None])
     # 2. growth
+    # formula to implement for all B
+    # r = y* rate of respiration for each B for all C
     r_growth = y_params*np.sum(r_resp,axis=0)
     # 3. mortality
+    # formula to implement for all B
+    # r = m*B
     r_mort = m_params*B
     # 4. dead microbes back to C (distributed evenly across all C)
-    r_hydrol = 0.1 * sum(r_mort)
+    # formula to implement for all dying B and add it to C
+    # r = sum of dying biomass
+    r_hydrol = 0.5*sum(r_mort)/c_n
     # 5. Complex DOM species added to simpler DOM species pools
+    # formula to implement for all C
+    # locate and order the C in decreasing order of simplicity
     simple_C = np.argsort(np.mean(kparams.reshape(dom_n, bio_n), axis=1))
+    # Add non-absorbed C fraction (not used for growth) into recycling fraction
+    # r = (1-y)*rate of respiration which will be unique for each B,C combination
     C_recycle = (1-y_params)*r_resp
+    # In the end we are interested in the bulk unused C product to recycle it to other C pools:
+    # r = sum(recycled pool for each C)
     C_recycle = np.sum(C_recycle, axis = 1)
 
     # define each ODE
     # 1. rate of change of carbon species concentration
-    C_rate = -1*np.sum(r_resp, axis = 1) + c_mol_bio*r_hydrol/c_n
+    C_rate = -1*np.sum(r_resp, axis = 1) + r_hydrol
     for ind in list(range(np.size(simple_C))):
-        if ind < np.size(simple_C)-1:
+        if ind < np.size(simple_C)-1: # No action to be taken for the most complex compound
+            # identify the index in C_rate for the simple C compound
             simp_c_ind = simple_C[ind] 
-            comp_c_ind = simple_C[ind+1] 
-            C_rate[simp_c_ind] = C_rate[simp_c_ind] + C_recycle[comp_c_ind]
-    
+            # identify the next level complex compound and its index
+            comp_c_ind = simple_C[ind+1]
+            # Add the unused fraction of next level complex compound to the simpler C compound
+            C_rate[simp_c_ind] += C_recycle[comp_c_ind]
+    # save microbial necromass into the carbon pool that is midway simple:
+    #mid_ind = simple_C[int(c_n/2)]
+    #C_rate[mid_ind] += r_hydrol
+
     # 2. rate of change of biomass species concentration
+    # Biomsdd grows and dies
     B_rate = r_growth - r_mort
     
     # Checking violation of conditions
+    # Check if concentration of carbon and biomass is tending to negative. If yes, reset it to 0.
     C_bad_ind = np.where(C<=0)
     B_bad_ind = np.where(B<=0)
     C_rate[C_bad_ind] = 0
@@ -69,44 +91,57 @@ def divers_carbon (data, c_n, b_n):
     B = data[:,c_n:]
     
     ## SHANNON DIVERSITY AND CARBON STOCK
-    Shannon = -np.sum(B/np.sum(B, axis = 0)*np.log(B/np.sum(B, axis = 0)), axis = 1)
-    C_stock = np.sum(C,axis=1) + 10*np.sum(B, axis=1)
+    proportion = B/np.sum(B,axis=0)
+    Shannon = -np.sum(proportion*np.log(proportion), axis = 1)
+    total_C_stock = np.sum(C,axis=1) + np.sum(B, axis=1)
+    C_stock = np.sum(C,axis=1)
 
-    return Shannon, C_stock
+
+    return Shannon, C_stock, total_C_stock
 
 #%%
 ## CONSTANTS SAMPLING
 #
 # Number of DOM/Carbon species:
 
-dom_n = np.random.randint(10,1000,1)[0]
-bio_n = np.random.randint(5,100,1)[0]
+dom_n = np.random.randint(4,10,1)[0]
+bio_n = np.random.randint(2,10,1)[0]
+print("Carbon species: ", dom_n)
+print("Biomass species: ", bio_n)
 # Initialize the same number of parameters:
 
-vparams = np.random.uniform(2,5,bio_n*dom_n)
+vparams = np.random.uniform(0.2,10,bio_n*dom_n)
 kparams = np.random.randint(1000,8000,bio_n*dom_n)
-yparams = np.random.ranf(bio_n)
+yparams = np.random.uniform(0.1,0.99,bio_n)
 mparams = np.mean(vparams.reshape(dom_n, bio_n),axis=0)/10
 
 # initial conditions
-dom_initial = np.random.randint(1000,10000,dom_n)
-biomass_initial = np.random.randint(5,50,bio_n)
+dom_initial = np.random.randint(500,1000,dom_n)
+biomass_initial = np.random.randint(10,100,bio_n)
 x0 = np.append(dom_initial,biomass_initial)
 
 # declare a time vector (time window)
-t = np.arange(0,100,0.01)
+t = np.arange(0,500,0.01)
 # solve
 x = odeint(rates, x0, t, args=(dom_n,bio_n))#, full_output=1)
 
+S, DOC, TOC = divers_carbon(x, dom_n, bio_n)
+
+## PLOT RESULTS
+
+# Time series of DOM concentration profile with biomass concnetration profile
 C = x[:,:dom_n]
 B = x[:,dom_n:]
 plt.plot(t,C, linestyle = "-", label = "DOM")
 plt.plot(t,B, linestyle = "--", label = "Biomass")
 #plt.legend()
-
-S, Cstock = divers_carbon(x, dom_n, bio_n)
+# Shannon diversity vs carbon stock
+plt.figure()
+plt.scatter(S, DOC)
+plt.xlabel ("Shannon")
+plt.ylabel ("DOC")
 
 plt.figure()
-plt.plot(S, Cstock)
+plt.scatter(S, TOC)
 plt.xlabel ("Shannon")
-plt.ylabel ("Carbon")
+plt.ylabel ("TOC")
