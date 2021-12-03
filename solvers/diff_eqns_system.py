2c9d7a2bd84504fd6edcd209bf9c533c996b87db
Skhurana <62461235+Skhurana136@users.noreply.github.com>
2021-12-03 16:25:19 +0100

    Updated script to sequentially add carbon pools to simpler carbon pools

--- solvers/diff_eqns_system.py
+++ solvers/diff_eqns_system.py
@@ -10,11 +10,12 @@ from scipy.integrate import odeint
 import numpy as np
 import matplotlib.pyplot as plt
 #%%
+c_mol_bio = 10 #carbon atoms in microbial necromass
 ## CONSOLIDATE SAMPLED PARAMETERS
 def call_constants():
     params = [vparams, kparams, yparams, mparams]
     return params
-# %%
+ # %%
 def rates(x,t, c_n, b_n):
     
     # constants
@@ -23,7 +24,6 @@ def rates(x,t, c_n, b_n):
     k_params = params[1]
     y_params = params[2]
     m_params = params[3]
-    c_mol_bio = 10 #carbon atoms in microbial necromass
 
     # assign each ODE to array elements
     C = x[:c_n]
@@ -40,6 +40,8 @@ def rates(x,t, c_n, b_n):
     r_hydrol = 0.1 * sum(r_mort)
     # 5. Complex DOM species added to simpler DOM species pools
     simple_C = np.argsort(np.mean(kparams.reshape(dom_n, bio_n), axis=1))
+    C_recycle = (1-y_params)*r_resp
+    C_recycle = np.sum(C_recycle, axis = 1)
 
     # define each ODE
     # 1. rate of change of carbon species concentration
@@ -48,7 +50,7 @@ def rates(x,t, c_n, b_n):
         if ind < np.size(simple_C)-1:
             simp_c_ind = simple_C[ind] 
             comp_c_ind = simple_C[ind+1] 
-            C_rate[simp_c_ind] = C_rate[simp_c_ind] + np.sum(r_resp[comp_c_ind])
+            C_rate[simp_c_ind] = C_rate[simp_c_ind] + C_recycle[comp_c_ind]
     
     # 2. rate of change of biomass species concentration
     B_rate = r_growth - r_mort
@@ -62,24 +64,33 @@ def rates(x,t, c_n, b_n):
     all_rates = np.append(C_rate, B_rate, axis = 0)
     return all_rates
 
+def divers_carbon (data, c_n, b_n):
+    C = data[:,:c_n]
+    B = data[:,c_n:]
+    
+    ## SHANNON DIVERSITY AND CARBON STOCK
+    Shannon = -np.sum(B/np.sum(B, axis = 0)*np.log(B/np.sum(B, axis = 0)), axis = 1)
+    C_stock = np.sum(C,axis=1) + 10*np.sum(B, axis=1)
+
+    return Shannon, C_stock
+
 #%%
 ## CONSTANTS SAMPLING
 #
 # Number of DOM/Carbon species:
 
-dom_n = np.random.randint(3,10,1)[0]
-bio_n = np.random.randint(2,10,1)[0]
+dom_n = np.random.randint(10,1000,1)[0]
+bio_n = np.random.randint(5,100,1)[0]
 # Initialize the same number of parameters:
 
-vparams = np.random.randint(2,10,bio_n*dom_n)
+vparams = np.random.uniform(2,5,bio_n*dom_n)
 kparams = np.random.randint(1000,8000,bio_n*dom_n)
 yparams = np.random.ranf(bio_n)
-mparams = np.mean(vparams.reshape(dom_n, bio_n),
-axis=0)/10
+mparams = np.mean(vparams.reshape(dom_n, bio_n),axis=0)/10
 
 # initial conditions
-dom_initial = np.random.randint(100,1000,dom_n)
-biomass_initial = np.random.randint(5,100,bio_n)
+dom_initial = np.random.randint(1000,10000,dom_n)
+biomass_initial = np.random.randint(5,50,bio_n)
 x0 = np.append(dom_initial,biomass_initial)
 
 # declare a time vector (time window)
@@ -93,21 +104,9 @@ plt.plot(t,C, linestyle = "-", label = "DOM")
 plt.plot(t,B, linestyle = "--", label = "Biomass")
 #plt.legend()
 
-#%%
-## SHANNON DIVERSITY AND CARBON STOCK
-S = -np.sum(B/np.sum(B, axis = 0)*np.log(B/np.sum(B, axis = 0)), axis = 1)
-Cresp = np.sum(C,axis=1)
+S, Cstock = divers_carbon(x, dom_n, bio_n)
 
 plt.figure()
-plt.plot(S, Cresp)
+plt.plot(S, Cstock)
 plt.xlabel ("Shannon")
 plt.ylabel ("Carbon")
-
-#%%
-simC = np.argsort(np.mean(kparams.reshape(dom_n, bio_n), axis=1))
-for ind in list(range(np.size(simC))):
-        if ind < np.size(simC)-1:
-            simp_c_ind = simC[ind] 
-            comp_c_ind = simC[ind+1] 
-            print(simp_c_ind, comp_c_ind)
-            C[simp_c_ind] = C[simp_c_ind] + C[comp_c_ind]
